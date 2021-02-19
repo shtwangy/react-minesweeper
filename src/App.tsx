d7b220ea7c4bc2ef3c26db711fb8a9a0ba1f892d
shtwangy <shtwangy@gmail.com>
2021-02-20 02:35:11 +0900

    feat(App): display config form

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import { ConfigForm } from "./components/ConfigForm/ConfigForm";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const App = () => {
+    const [configDraft, setConfigDraft] = useState({
+            width: 12,
+            height: 12,
+            rate: 0.1,
+        }
+    );
+    const [config, setConfig] = useState(() => configDraft);
+    return (
+    <div>
+      <ConfigForm
+          config={config}
+      />
     </div>
   );
 }
